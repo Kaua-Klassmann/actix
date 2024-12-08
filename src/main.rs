46735e05f5e6e4e0f39b99ba939479df54fb5db1
Kauã Klassmann <kaua.klassmann661@gmail.com>
2024-12-08 10:16:07 -0300

    config

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use actix_web::{App, HttpServer};
+use actix_cors::Cors;
+
+mod handlers;
+mod routes;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()>{
+    HttpServer::new(|| {
+        App::new()
+            .wrap(
+                Cors::default()
+                    .allow_any_origin()
+                    .allow_any_method()
+                    .allow_any_header()
+            )
+            .configure(routes::static_routes::configure_static_routes)
+            .configure(routes::path_routes::configure_path_routes)
+    })
+    .bind(("127.0.0.1", 3000))?
+    .run()
+    .await
+}
