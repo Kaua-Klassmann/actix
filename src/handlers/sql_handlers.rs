bcb132544ffb966af6473d9ea37273e319bf7142
Kauã Klassmann <kaua.klassmann661@gmail.com>
2024-12-21 22:27:02 -0300

    updated in sql handler to generic database

--- src/handlers/sql_handlers.rs
+++ src/handlers/sql_handlers.rs
@@ -1,12 +1,12 @@
 use actix_web::{web::Data, HttpResponse, Responder};
-use sea_orm::{ConnectionTrait, DatabaseBackend::MySql, Statement};
+use sea_orm::{ConnectionTrait, Statement};
 
 use crate::config::app_state::AppState;
 
 pub async fn sql(app_data: Data<AppState>) -> impl Responder {
     let _res = app_data.db
         .query_all(
-            Statement::from_string(MySql, "SELECT * FROM user")
+            Statement::from_string(app_data.db.get_database_backend(), "SELECT * FROM user")
         ).await.unwrap();
 
     dbg!(_res);
